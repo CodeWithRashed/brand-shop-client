3dfc3cbd5e0f1e0eeaa29fcc3157fa79f05b5b22
Md Rashed Ali <tools.rashed@gmail.com>
2023-10-18 06:14:53 +0600

    404 page added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,11 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import HomePage from "../Pages/HomePage";
 import AuthPage from "../Pages/AuthPage";
+import Error404 from '../Pages/Error404';
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    errorElement: <>You are on wrong route</>,
+    errorElement: <Error404></Error404>,
     children: [
       {
         path: "/",
